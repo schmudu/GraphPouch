de27d2fd53b24f8d624bc5e9b4fe8560f26a4410
Patrick Lee <patrick.robin.lee@gmail.com>
2012-07-20 18:44:52 -0400

    first commit

---
+++ GraphPouch/EDDocument.h
@@ -0,0 +1,13 @@
+//
+//  EDDocument.h
+//  GraphPouch
+//
+//  Created by PATRICK LEE on 7/20/12.
+//  Copyright (c) 2012 Patrick Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EDDocument : NSPersistentDocument
+
+@end
---
+++ GraphPouch/EDDocument.m
@@ -0,0 +1,40 @@
+//
+//  EDDocument.m
+//  GraphPouch
+//
+//  Created by PATRICK LEE on 7/20/12.
+//  Copyright (c) 2012 Patrick Lee. All rights reserved.
+//
+
+#import "EDDocument.h"
+
+@implementation EDDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"EDDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
++ (BOOL)autosavesInPlace
+{
+    return YES;
+}
+
+@end
