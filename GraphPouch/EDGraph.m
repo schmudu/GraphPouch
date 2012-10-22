114a703b1c878d76fddafb822f9924f251b502c9
Patrick Lee <patrick.robin.lee@gmail.com>
2012-10-22 12:39:35 -0400

    Added encoding for element attributes.

--- GraphPouch/EDGraph.m
+++ GraphPouch/EDGraph.m
@@ -23,6 +23,11 @@
         [self setEquation:[[aDecoder decodeObjectForKey:EDGraphAttributeEquation] string]];
         [self setHasGridLines:[aDecoder decodeBoolForKey:EDGraphAttributeGrideLines]];
         [self setHasTickMarks:[aDecoder decodeBoolForKey:EDGraphAttributeTickMarks]];
+        [self setSelected:[aDecoder decodeBoolForKey:EDElementAttributeSelected]];
+        [self setLocationX:[aDecoder decodeFloatForKey:EDElementAttributeLocationX]];
+        [self setLocationY:[aDecoder decodeFloatForKey:EDElementAttributeLocationY]];
+        [self setElementWidth:[aDecoder decodeFloatForKey:EDElementAttributeWidth]];
+        [self setElementHeight:[aDecoder decodeFloatForKey:EDElementAttributeHeight]];
     }
     return self;
 }
@@ -31,5 +36,10 @@
     [aCoder encodeObject:[self equation] forKey:EDGraphAttributeEquation];
     [aCoder encodeBool:[self hasGridLines] forKey:EDGraphAttributeGrideLines];
     [aCoder encodeBool:[self hasTickMarks] forKey:EDGraphAttributeTickMarks];
+    [aCoder encodeBool:[self selected] forKey:EDElementAttributeSelected];
+    [aCoder encodeFloat:[self locationX] forKey:EDElementAttributeLocationX];
+    [aCoder encodeFloat:[self locationY] forKey:EDElementAttributeLocationY];
+    [aCoder encodeFloat:[self elementWidth] forKey:EDElementAttributeWidth];
+    [aCoder encodeFloat:[self elementHeight] forKey:EDElementAttributeHeight];
 }
 @end
