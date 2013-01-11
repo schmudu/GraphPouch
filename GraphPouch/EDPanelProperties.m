626662501986511cd1b59bd4a34987076f1aa641
Patrick Lee <patrick.robin.lee@gmail.com>
2013-01-11 18:57:29 -0500

    Save shortcut works.

---
+++ GraphPouch/EDPanelProperties.m
@@ -0,0 +1,13 @@
+//
+//  EDPanelProperties.m
+//  GraphPouch
+//
+//  Created by PATRICK LEE on 1/11/13.
+//  Copyright (c) 2013 Patrick Lee. All rights reserved.
+//
+
+#import "EDPanelProperties.h"
+
+@implementation EDPanelProperties
+
+@end
