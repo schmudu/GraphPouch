0b99491a52927a5ec6cbb743c00dd928fa1f6c68
Patrick Lee <patrick.robin.lee@gmail.com>
2013-01-13 08:46:50 -0500

    Got formatter for equation editor to work.

---
+++ GraphPouch/EDFormatterEquation.h
@@ -0,0 +1,13 @@
+//
+//  EDFormatterEquation.h
+//  GraphPouch
+//
+//  Created by PATRICK LEE on 1/13/13.
+//  Copyright (c) 2013 Patrick Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EDFormatterEquation : NSFormatter
+
+@end
