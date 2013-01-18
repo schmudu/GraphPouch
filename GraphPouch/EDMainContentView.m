328497d4ce8dff9f0c2f46ad8a6f39d2fca34c48
Patrick Lee <patrick.robin.lee@gmail.com>
2013-01-17 23:06:38 -0500

    Closer to getting auto center to work.

--- GraphPouch/EDMainContentView.m
+++ GraphPouch/EDMainContentView.m
@@ -45,11 +45,33 @@
     for (NSView *view in [self subviews]){
         if ([view isKindOfClass:[EDWorksheetScrollView class]]){
             // set new position based on constant width of document and new width
-            float newOriginX = ([view frame].size.width - EDWorksheetViewWidth)/2;
-            float newOriginY = ([view frame].size.height - EDWorksheetViewHeight)/2;
+            float worksheetWidth = [view frame].size.width;
+            float worksheetHeight = [view frame].size.height;
             
+            /*
+            if (EDWorksheetViewHeight > [self frame].size.height)
+                worksheetHeight = EDWorksheetViewWidth;
+            else
+                worksheetHeight = [self frame].size.height;
+            
+            if (EDWorksheetViewWidth > [self frame].size.width)
+                worksheetWidth = EDWorksheetViewWidth;
+            else
+                worksheetWidth = [self frame].size.width;
+            */
+            //float newOriginX = ([view frame].size.width - [self frame].size.width)/2;
+            //float newOriginY = ([view frame].size.height - [self frame].size.height)/2;
+            float newOriginX = ([self frame].size.width - worksheetWidth)/2;
+            float newOriginY = ([self frame].size.height - worksheetHeight)/2;
+            
+            // constraints
+            /*
+            if (newOriginY < 10){
+                newOriginY = 10;
+            }*/
             [view setFrameOrigin:NSMakePoint(newOriginX, newOriginY)];
-            NSLog(@"x:%f y%f scroll width:%f self width:%f", [view frame].origin.x, [self frame].origin.y, [view frame].size.width, [self frame].size.width);
+            //NSLog(@"self y:%f x:%f y%f scroll width:%f self width:%f", [self frame].origin.y, [view frame].origin.x, [self frame].origin.y, [view frame].size.width, [self frame].size.width);
+            NSLog(@"self y:%f self height:%f scroll y:%f scroll height:%f window height:%f", [self frame].origin.y, [self frame].size.height, [view frame].origin.y, [view frame].size.height, [[self window] frame].size.height);
         }
     }
 }
