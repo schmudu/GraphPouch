056a9f8dc23dbd4c80a61d4e9b9107ed1e29b4eb
Patrick Lee <patrick.robin.lee@gmail.com>
2013-01-29 22:29:12 -0500

    Undo works a thousand times.

--- GraphPouch/EDCoreDataUtility.m
+++ GraphPouch/EDCoreDataUtility.m
@@ -26,9 +26,7 @@
     [childContext setParentContext:rootContext];
     
     // set undo
-    [childContext setUndoManager:[startContext undoManager]];
-    
-    NSLog(@"root context:%@ child context:%@ start context undo mgr:%@", rootContext, childContext, [startContext undoManager]);
+    [rootContext setUndoManager:[startContext undoManager]];
     
     [contexts setObject:childContext forKey:EDKeyContextChild];
     [contexts setObject:rootContext forKey:EDKeyContextRoot];
