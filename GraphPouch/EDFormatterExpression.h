1d6fbe226133757051ce08321e47e4f7d891c0dd
Patrick Lee <patrick.robin.lee@gmail.com>
2013-04-21 07:02:10 -0400

    Added formatter for expression panel.

---
+++ GraphPouch/EDFormatterExpression.h
@@ -0,0 +1,13 @@
+//
+//  EDFormatterExpression.h
+//  GraphPouch
+//
+//  Created by PATRICK LEE on 4/21/13.
+//  Copyright (c) 2013 Patrick Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EDFormatterExpression : NSFormatter
+
+@end
---
+++ GraphPouch/EDFormatterExpression.m
@@ -0,0 +1,46 @@
+//
+//  EDFormatterExpression.m
+//  GraphPouch
+//
+//  Created by PATRICK LEE on 4/21/13.
+//  Copyright (c) 2013 Patrick Lee. All rights reserved.
+//
+
+#import "EDFormatterExpression.h"
+
+@implementation EDFormatterExpression
+- (NSString *)stringForObjectValue:(id)obj{
+    return [NSString stringWithFormat:@"%@", obj];
+}
+
+- (BOOL)getObjectValue:(__autoreleasing id *)obj forString:(NSString *)string errorDescription:(NSString *__autoreleasing *)error{
+    *obj = [[NSString alloc] initWithFormat:@"%@", string];
+    return TRUE;
+}
+
+- (BOOL)isPartialStringValid:(NSString *__autoreleasing *)partialStringPtr proposedSelectedRange:(NSRangePointer)proposedSelRangePtr originalString:(NSString *)origString originalSelectedRange:(NSRange)origSelRange errorDescription:(NSString *__autoreleasing *)error{
+    NSError *regexError = NULL;
+    // regular expression for the following items
+    // numbers (negative, unsigned, signed, decimal form
+    // trigonometric functions
+    // powers
+    // operators
+    // identifiers
+    // parenthesis
+    /*NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^(|[0-9]|\\.|s|i|n|c|o|s|t|a|\\^|\\+|\\-|\\*|\\/|x|y|\\(|\\))*$"
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&regexError];*/
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^(|[0-9]|\\.|\\^|\\+|\\-|\\=|\\*|\\/|x|y|\\(|\\))*$"
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&regexError];
+    NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:*partialStringPtr options:0 range:NSMakeRange(0, [*partialStringPtr length])];
+    
+    if (rangeOfFirstMatch.location != NSNotFound) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+
+@end
